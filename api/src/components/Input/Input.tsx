7e71656e18ec2dd107ca434bc6f5b670880ca978
JEDIBASIL <jedidiahbasil@gmail.com>
2024-04-28 21:56:10 +0100

    today class

--- api/src/components/Input/Input.tsx
+++ api/src/components/Input/Input.tsx
@@ -1,18 +1,21 @@
 import "./input.css";
- interface Iinput{
-    label: string
-    type: "text" | "password" | "email" | "number" 
- }
+interface Iinput
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label: string;
+}
 
-const Input = ({label, type}:Iinput) => {
+const Input = ({ label, type, ...props }: Iinput) => {
   return (
     <>
       <div className="inputWrapper">
         <label htmlFor="">{label}</label>
-      <input type={type} />
-    </div>
+        <input type={type} {...props} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
